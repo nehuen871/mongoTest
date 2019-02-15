76bb85931325cf72ab2001587c0de4cd44228a2f
nehuen871 <nehuennavarro@gmail.com>
2019-02-15 14:35:04 -0300

    first commit

---
+++ mongoUniversiti/m121/project_stage.js
@@ -0,0 +1,33 @@
+// project ``name`` and remove ``_id``
+db.solarSystem.aggregate([{ "$project": { "_id": 0, "name": 1 } }]);
+
+// project ``name`` and ``gravity`` fields, including default ``_id``
+db.solarSystem.aggregate([{ "$project": { "name": 1, "gravity": 1 } }]);
+
+// using dot-notation to express the projection fields
+db.solarSystem.aggregate([{ "$project": { "_id": 0, "name": 1, "gravity.value": 1 } }]);
+
+// reassing ``gravity`` field with value from ``gravity.value`` embeded field
+db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "gravity": "$gravity.value" }}]);
+
+// creating a document new field ``surfaceGravity``
+db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "surfaceGravity": "$gravity.value" }}]);
+
+// creating a new field ``myWeight`` using expressions
+db.solarSystem.aggregate([{"$project": { "_id": 0, "name": 1, "myWeight": { "$multiply": [ { "$divide": [ "$gravity.value", 9.8 ] }, 86 ] } }}]);
+
+
+db.movies.aggregate(pipe).itcount()
+
+{"$project": { "_id": 0, "titlemovi": {$split: ["$title", " "]}}}
+
+var pipe = [{"$project": { "_id": 0,"tamaño":{$size:{"titlemovi": {$split: ["$title", " "]}}}}}]
+
+
+"tamaño":{$size:{"$titlemovi": {$split: ["$title", " "]}}}
+
+"titlemovi": {$split: ["$title", " "]}
+
+
+var pipe = [{"$project": { "_id": 0, "titlemovi": {$split: ["$title", " "]}}}]
+var pipe = [{"$project": { "_id": 0, "titlemovi": {"$size":{"$split": ["$title", " "]}}}},{$match: {"titlemovi":{$eq:1}}}]
