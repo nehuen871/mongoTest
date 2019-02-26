6563821aa8466caf543044c68e235a1a7a4e171a
nehuen871 <nehuennavarro@gmail.com>
2019-02-26 18:09:18 -0300

    Update moviesDAO.js
    test

--- mongoUniversiti/m220/mflix-js/src/dao/moviesDAO.js
+++ mongoUniversiti/m220/mflix-js/src/dao/moviesDAO.js
@@ -195,6 +195,9 @@ export default class MoviesDAO {
     const queryPipeline = [
       matchStage,
       sortStage,
+      skipStage,
+      limitStage,
+      facetStage,
       // TODO Ticket: Faceted Search
       // Add the stages to queryPipeline in the correct order.
     ]
@@ -260,7 +263,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.limit(moviesPerPage).skip(moviesPerPage*page);
 
     try {
       const moviesList = await displayCursor.toArray()
